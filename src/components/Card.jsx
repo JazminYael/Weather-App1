6ab8abded992d68653834271178492bd6cff1440
Jael Batista <78515537+JazminYael@users.noreply.github.com>
2021-10-21 01:11:24 -0300

    Add validation in props of card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CardTemp from "./CardTemp";
+import PropTypes from "prop-types";
 
 export default function Card({ max, min, name, img, onClose }) {
 	// acá va tu código
@@ -11,11 +12,20 @@ export default function Card({ max, min, name, img, onClose }) {
 		<div>
 			<button onClick={handleOnclose}>x</button>
 			<span>{name}</span>
-      <CardTemp label="Min" value={min} />
-      <CardTemp label="Max" value={max} />
-			<img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="icono del clima" />
+			<CardTemp label="Min" value={min} />
+			<CardTemp label="Max" value={max} />
+			<img
+				src={`http://openweathermap.org/img/wn/${img}@2x.png`}
+				alt="icono del clima"
+			/>
 		</div>
 	);
 }
 
-
+Card.propTypes = {
+	max: propTypes.number,
+	min: propTypes.number,
+	name: Proptypes.string,
+	img: Proptypes.string,
+	onClose: Proptypes.func,
+};
