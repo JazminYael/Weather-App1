4a14ded8cf20b5a9e9a5cf146350813e1c713f34
Jael Batista <78515537+JazminYael@users.noreply.github.com>
2021-10-21 21:13:52 -0300

    upload

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import CardTemp from "./CardTemp";
 import PropTypes from "prop-types";
+import styles from "./Card.module.css";
+import { IoCloseCircleOutline } from "react-icons/io5";
 
 export default function Card({ max, min, name, img, onClose }) {
 	// acá va tu código
@@ -9,9 +11,11 @@ export default function Card({ max, min, name, img, onClose }) {
 	}
 
 	return (
-		<div>
-			<button onClick={handleOnclose}>x</button>
-			<span>{name}</span>
+		<div className={styles.card}>
+			<button className={styles.closeButton} onClick={handleOnclose}>
+				<IoCloseCircleOutline/>
+			</button>
+			<span className={styles.cityName}>{name}</span>
 			<CardTemp label="Min" value={min} />
 			<CardTemp label="Max" value={max} />
 			<img
